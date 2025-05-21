f210f3bc9f1e7c536e88bae62082f05a19d897f9
Antoni Michalczak <antoni0michalczak@gmail.com>
2025-05-21 18:23:40 +0200

    setup workshopservice

---
+++ workshop-service/WorkshopService/EnvConfig.cs
@@ -0,0 +1,18 @@
+﻿using dotenv.net;
+
+namespace AuthService;
+
+public class EnvConfig
+{
+    public string DbConnectionString { get; }
+
+    public EnvConfig()
+    {
+        DotEnv.Load();
+
+        DbConnectionString = Environment.GetEnvironmentVariable("POSTGRES_CONN_STR");
+        if (string.IsNullOrWhiteSpace(DbConnectionString))
+            throw new Exception("Brak lub pusta zmienna środowiskowa POSTGRES_CONN_STR!");
+        
+    }
+}
--- workshop-service/WorkshopService/Program.cs
+++ workshop-service/WorkshopService/Program.cs
@@ -1,16 +1,49 @@
-using WorkshopService.Services;
+using AuthService;
+using AuthService.Data;
+using AuthService.Services;
+using Microsoft.EntityFrameworkCore;
+
+var config = new EnvConfig();
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(config.DbConnectionString));
+
+// Dodaj usługi do kontenera DI
+builder.Services.AddControllers();
 builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
+
+// Dodaj Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGet("/",
-    () =>
-        "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    if (!await db.Database.CanConnectAsync())
+        throw new Exception("❌ Cannot connect to database");
+}
+
+
+
+// Konfiguracja HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.MapGrpcReflectionService();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+// Mapowanie kontrolerów API
+app.MapControllers();
+
+// Mapowanie serwisów gRPC
+app.MapGrpcService<AuthServiceImpl>();
 
 app.Run();
